39b93d8b459a6e6408be62e1b373127772fff382
RagyScripts <119745291+R4nzy@users.noreply.github.com>
2023-12-30 11:20:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('DownloadExternal').addEventListener('click', function () {
+    window.location.href = 'https://www.mediafire.com/file/9orfb7zw13ues87/GX_External.rar/file'; // replace with your desired URL
+});
+
+document.getElementById('DownloadInternal').addEventListener('click', function () {
+    window.location.href = ''; // replace with your desired URL
+});
+
+document.getElementById('DiscordServer').addEventListener('click', function () {
+    window.location.href = 'https://discord.gg/YR6v8YyK4S'; // replace with your desired URL
+});
